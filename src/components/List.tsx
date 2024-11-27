43d29534ed093e66bdde12877c4343ca26949d95
Philip Huynh <wuflerz@outlook.com>
2024-11-27 12:54:33 +0200

    Combine offers when each has 1 game

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { format } from 'date-fns'
 import {
 	Card,
@@ -93,7 +93,9 @@ export default function List({ games }: { games: any }) {
 								alt={game.title}
 								width={640}
 								height={360}
-								className={`w-full h-48 lg:h-60 object-cover transition-all duration-300 group-hover:scale-105 ${
+								className={`w-full ${
+									isSingleGame ? 'h-80' : 'h-48 lg:h-60'
+								} object-cover transition-all duration-300 group-hover:scale-105 ${
 									timeLeft[game.id] === 'Expired' ? 'grayscale' : ''
 								}`}
 							/>
@@ -163,8 +165,11 @@ export default function List({ games }: { games: any }) {
 		)
 	}
 
+	const isSingleGame =
+		games.currentGames.length === 1 && games.nextGames.length === 1
+
 	const renderGameList = (games: any[], isCurrentGames: boolean) => (
-		<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+		<div className={`grid grid-cols-1 xl:grid-cols-3 gap-6`}>
 			{games.length > 0 ? (
 				games.map((game: any) => renderGameCard(game, isCurrentGames))
 			) : (
@@ -172,6 +177,23 @@ export default function List({ games }: { games: any }) {
 					Failed to fetch offers. Check back later or check out the official site.
 				</p>
 			)}
+		</div>
+	)
+
+	const combinedView = () => (
+		<div className="grid grid-cols-2 gap-6">
+			<div>
+				<h2 className="text-3xl font-bold mb-4 text-epic-blue dark:text-epic-blue">
+					Current
+				</h2>
+				{games.currentGames.map((game: any) => renderGameCard(game, true))}
+			</div>
+			<div>
+				<h2 className="text-3xl font-bold mb-4 text-epic-lightBlue dark:text-white">
+					Upcoming
+				</h2>
+				{games.nextGames.map((game: any) => renderGameCard(game, false))}
+			</div>
 		</div>
 	)
 
@@ -191,18 +213,24 @@ export default function List({ games }: { games: any }) {
 			</Tabs>
 
 			<div className="hidden lg:block">
-				<section>
-					<h2 className="text-3xl font-bold mb-4 text-epic-blue dark:text-epic-blue">
-						Current
-					</h2>
-					{renderGameList(games.currentGames, true)}
-				</section>
-				<section className="mt-8">
-					<h2 className="text-3xl font-bold mb-4 text-epic-lightBlue dark:text-white">
-						Upcoming
-					</h2>
-					{renderGameList(games.nextGames, false)}
-				</section>
+				{isSingleGame ? (
+					<section>{combinedView()}</section>
+				) : (
+					<>
+						<section>
+							<h2 className="text-3xl font-bold mb-4 text-epic-blue dark:text-epic-blue">
+								Current
+							</h2>
+							{renderGameList(games.currentGames, true)}
+						</section>
+						<section className="mt-8">
+							<h2 className="text-3xl font-bold mb-4 text-epic-lightBlue dark:text-white">
+								Upcoming
+							</h2>
+							{renderGameList(games.nextGames, false)}
+						</section>
+					</>
+				)}
 			</div>
 		</div>
 	)
