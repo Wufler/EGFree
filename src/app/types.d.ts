b6f81f93462ba8074ba22986f583be6925cf841f
Philip Huynh <wuflerz@outlook.com>
2025-05-01 18:00:04 +0300

    lint

--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -92,34 +92,3 @@ type EgFreeSettings = {
     webhookUrl: string
     showDiscordPreview: boolean
 }
-
-type Field = {
-    name: string;
-    value: string;
-    inline: boolean;
-};
-
-type Embed = {
-    color: number;
-    fields: Field[];
-    author?: {
-        name?: string;
-        url?: string;
-        icon_url?: string;
-    };
-    footer?: {
-        text?: string;
-    };
-    timestamp?: string;
-    image?: {
-        url: string;
-    };
-};
-
-type EmbedData = {
-    content: string;
-    embeds: Embed[];
-    username: string;
-    avatar_url: string;
-    attachments: any[];
-};
